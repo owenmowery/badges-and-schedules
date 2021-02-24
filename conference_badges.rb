b86dc089a1501e61bd5505d11ded748c9f9660b8
Owen Mowery <owenmowery99@gmail.com>
2021-02-24 14:52:30 -0500

    commit tree 673ce1743bb1a6ee22b5a6ee749b7b89e021f79f

--- conference_badges.rb
+++ conference_badges.rb
@@ -1,1 +1,32 @@
-# Write your code here.
+require 'pry'
+def badge_maker(name)
+    return "Hello, my name is #{name}."
+end
+
+def batch_badge_creator(array)
+    new_array = []
+    array.each do |name|
+        new_array << "Hello, my name is #{name}."
+    end
+    return new_array
+end
+
+def assign_rooms(array)
+    new_array = []
+    array.each_with_index do |name, index|
+        new_array << "Hello, #{name}! You'll be assigned to room #{index + 1}!"
+    end
+    return new_array
+end
+
+def printer(attendees)
+
+    result = batch_badge_creator(attendees)
+    result.each do |x|
+        puts x
+    end
+    result_2 = assign_rooms(attendees)
+    result_2.each do |x|
+        puts x
+    end 
+end
